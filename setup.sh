8035a52114b188354f45093ad85fae349f6fa34c
I-Fun <i.fuunn@gmail.com>
2023-08-30 19:47:27 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-Thamrin=Thamrin-Connection-Monitoring
-echo "Installing $Thamrin" ; git clone https://github.com/louislam/uptime-kuma.git $Thamrin > /dev/null
-sed -i 's/louislam/I-Fun/' $Thamrin/extra/download-dist.js ; sed -i 's/uptime-kuma/Thamrin-Connection-Monitoring/' $Thamrin/extra/download-dist.js
-cd $Thamrin
-echo "Please Wait..."
-npm run setup > /dev/null 2>&1
-sed -i 's/3001/5001/' server/server.js
-pm2 start server/server.js --name Thamrin-Connection
